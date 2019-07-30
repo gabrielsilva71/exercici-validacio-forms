eaa9e9cb910e34a6228e3b0797a4aec1c203207c
gabrielsilva71 <gabrielsilva71@gmail.com>
2019-07-30 10:43:07 +0200

    1. script.js actualizado - fórmulas separadas el campo de búsqueda y el formulario de registro/login. 2. páginas html actualizadas - adicionados IDs a los formularios para evitar conflictos con el JS.

--- js/script.js
+++ js/script.js
@@ -1,10 +1,19 @@
-var form = document.querySelector('#registro');
+      var form1 = document.querySelector('#busqueda');
 
-form.addEventListener('submit', function(event) {
-	if (form.checkValidity() == false) {
-		event.preventDefault();
-		event.stopPropagation();
-	}
-	form.classList.add('was-validated');
-});
+      form1.addEventListener('submit', function(event) {
+        if (form1.checkValidity() === false) {
+          event.preventDefault();
+          event.stopPropagation();
+        }
+        form1.classList.add('was-validated');
+      })
 
+      var form2 = document.querySelector('#form');
+
+      form2.addEventListener('submit', function(event) {
+        if (form2.checkValidity() === false) {
+          event.preventDefault();
+          event.stopPropagation();
+        }
+        form2.classList.add('was-validated');
+      })
